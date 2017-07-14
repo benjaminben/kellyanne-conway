1cf3c9968298018af95564df3f36556aae12ee46
benBen <benmbenjamin@gmail.com>
2017-07-13 22:40:18 -0700

    parallaxes

--- js/app.js
+++ js/app.js
@@ -11,6 +11,18 @@
 
   var shiftCarousel = function(target) {
     carousel.slideIndex = target
+    if (carousel.slideIndex === 0) {
+      carousel.backBtn.className += ' passive'
+    } else if (carousel.slideIndex === carousel.main.slides.length - 1) {
+      carousel.nextBtn.className += ' passive'
+    } else {
+      if (!!carousel.backBtn.className.match(/passive/)) {
+        carousel.backBtn.className = carousel.backBtn.className.replace(/passive/, '')
+      }
+      if (!!carousel.nextBtn.className.match(/passive/)) {
+        carousel.nextBtn.className = carousel.nextBtn.className.replace(/passive/, '')
+      }
+    }
     carousel.main.style.transform = 'translate(-' + (target * 100) + '%, 0)';
   }
 
@@ -34,6 +46,25 @@
       var target = window.parseInt(slide.getAttribute('data-index'))
       shiftCarousel(target)
     })
+  })
+
+  shiftCarousel(carousel.slideIndex)
+
+
+  // parallax
+  var scroller = new ScrollMagic.Controller()
+  var llaxes = Array.from(document.querySelectorAll('.llax'))
+
+  llaxes.forEach(function(llax) {
+    new ScrollMagic.Scene({
+      triggerElement: llax,
+      triggerHook: 1,
+      duration: '100%'
+    })
+    .setTween(TweenMax.from(llax, 1, {
+      yPercent: 12,
+    }))
+    .addTo(scroller)
   })
 
 })()
