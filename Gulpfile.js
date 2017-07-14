5c532cd0d497f64ebe04cb5887160237a3f08213
benBen <benmbenjamin@gmail.com>
2017-07-13 18:31:56 -0700

    various mobiles

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,15 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
+var gulp         = require('gulp')
+var sass         = require('gulp-sass')
+var postcss      = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('styles', function() {
   gulp.src('./css/sass/**/*.scss')
       .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.init())
+      .pipe(postcss([ autoprefixer() ]))
+      .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest('./css'))
 })
 
